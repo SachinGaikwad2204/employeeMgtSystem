6c61ba5d1b4a5d37adaa24536f10908e6692a0a9
Sachin Gaikwad <sachingaikwad2204@gmail.com>
2025-11-03 02:20:51 +0530

    Update API_BASE to localhost:8080

--- src/employeeService.js
+++ src/employeeService.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 // During local development, this will default to 'http://localhost:8080'.
 // When deployed, you MUST set the REACT_APP_API_BASE environment variable
 // in your hosting service (e.g., Vercel) to the public URL of your deployed backend.
-const API_BASE = process.env.REACT_APP_API_BASE || 'https://isagogically-ghostliest-pura.ngrok-free.dev';
+const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:8080';
 const BASE_URL = `${API_BASE.replace(/\/$/, '')}/api/employees`;
 
 export const getAllEmployees = () => axios.get(BASE_URL);
